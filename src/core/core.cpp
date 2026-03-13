c1d45d093c9ed278375061e59ea74ff4a0833ad1
Dylan Leothaud <mail@dleothaud.fr>
2026-03-13 19:28:26 +0100

    add main core module

---
+++ src/core/core.cpp
@@ -0,0 +1,33 @@
+/* Copyright 2026 Dylan Leothaud
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+export module core;
+
+export import :allocs;
+export import :cstring;
+export import :errors;
+export import :functions;
+export import :initializerList;
+export import :map;
+export import :set;
+export import :sharedPtr;
+export import :start;
+export import :stream;
+export import :string;
+export import :stringLiteral;
+export import :syscalls;
+export import :traits;
+export import :types;
+export import :uniquePtr;
+export import :variant;
+export import :vector;
