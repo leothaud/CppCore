c135b053578aeffd72cd0b7efd9c8d71cb4001c3
Dylan Leothaud <mail@dleothaud.fr>
2026-03-13 19:22:47 +0100

    add types definition using predefined macros

---
+++ src/core/types.cpp
@@ -0,0 +1,24 @@
+/* Copyright 2026 Dylan Leothaud
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+export module core:types;
+
+export using u64 = __UINT64_TYPE__;
+export using u32 = __UINT32_TYPE__;
+export using u16 = __UINT16_TYPE__;
+export using u8 = __UINT8_TYPE__;
+
+export using i64 = __INT64_TYPE__;
+export using i32 = __INT32_TYPE__;
+export using i16 = __INT16_TYPE__;
+export using i8 = __INT8_TYPE__;
