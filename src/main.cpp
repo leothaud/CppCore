fd6ddc5454d97fc1fc8d682278a5466b45e85c33
Dylan Leothaud <mail@dleothaud.fr>
2026-03-28 09:31:39 +0100

    Add a not-yet functionnal "unmangle" function to translate c++ symbols into readable names.

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,32 @@
 import core;
 
-int fTest() {
-  core::getThreadLogger()->info("Yo!");
-  core::sleep(2);
-  core::getThreadLogger()->info("End.");
-  return 0;
+struct Base {
+  virtual void doPrint() {};
+};
+
+struct A : public Base {
+  void doPrint() { core::getThreadLogger()->info("A"); }
+};
+
+struct B : public Base {
+  virtual void doPrint() override { core::getThreadLogger()->info("B"); }
+};
+
+void go(Base *base) {
+  // if (auto *a = dynamic_cast<A *>(base)) {
+  //   a->doPrint();
+  // } else {
+  base->doPrint();
+  // }
 }
 
 int main() {
-  core::sout << "Yo " << core::getColor<core::TEXT_COLORS::RED>("toi") << " !\n"
-             << 0x123_x << "\n";
+  A a;
+  go(&a);
+  // core::sout << "Yo " << core::getColor<core::TEXT_COLORS::RED>("toi") << "
+  // !\n"
+  //            << core::getColor<core::TEXT_COLORS::BLUE>(0x123_x) << " - " <<
+  //            12
+  //            << "\n";
   return 0;
 }
